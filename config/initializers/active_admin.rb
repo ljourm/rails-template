0ee74d21c548e1e54dd4de4eb313d16248905682
ljourm <ljourm@gmail.com>
2020-02-08 20:55:32 +0900

    feat: 運用を想定したactive admin設定に変更

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Rails Template"
+  config.site_title = "Active Admin"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -34,6 +34,7 @@ ActiveAdmin.setup do |config|
   #
   # Default:
   # config.default_namespace = :admin
+  config.default_namespace = :acadmin
   #
   # You can customize the settings for each namespace by using
   # a namespace block. For example, to change the site title
