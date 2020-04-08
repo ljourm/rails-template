7f18537e8d8f4395f3749278ac0e7491d756890c
ljourm <ljourm@gmail.com>
2020-04-08 23:25:02 +0900

    feat: railsが初回生成する不要ファイルの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,3 @@
-// require("@rails/ujs").start()
-// require("@rails/activestorage").start()
-// require("channels")
-
 import "../stylesheets/application"
 import "@fortawesome/fontawesome-free/js/all"
 
