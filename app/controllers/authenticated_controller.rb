a33dcfbb0f1f20e2834b9dec708c8252787343fd
ljourm <ljourm@gmail.com>
2020-04-22 01:48:11 +0900

    feat: logrageの改良(rescueしたexceptionを出力できるように。トレース件数を変更。user_idの追加)

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,3 +1,8 @@
 class AuthenticatedController < ApplicationController
   before_action :authenticate_user!
+
+  def append_info_to_payload(payload)
+    super
+    payload[:user_id] = current_user&.id
+  end
 end
