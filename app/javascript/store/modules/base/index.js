3791fe30cecb63f58c0cb4c4fcd1a3fee344f6d7
ljourm <ljourm@gmail.com>
2020-04-24 22:39:30 +0900

    feat: store/baseに404エラーを追加

--- app/javascript/store/modules/base/index.js
+++ app/javascript/store/modules/base/index.js
@@ -8,6 +8,7 @@ import api from "lib/api"
 
 const ERROR_MESSAGE_DEFAULT =
   "エラーが発生しました。管理者にお問い合わせください。"
+const ERROR_MESSAGE_NOT_FOUND = "お探しのページは見つかりません。"
 const ERROR_MESSAGE_CONNECTION_FAILED =
   "通信エラーが発生しました。ネットワーク環境を確認し、再度実行してください。このメッセージが繰り返し表示される場合は管理者までお問い合わせください。"
 
@@ -44,6 +45,8 @@ const actions = {
         if (error.response.status === 401) {
           dispatch("redirectToLogin")
           return
+        } else if (error.response.status === 404) {
+          message = ERROR_MESSAGE_NOT_FOUND
         } else if (error.response.message) {
           message = error.response.message
         }
