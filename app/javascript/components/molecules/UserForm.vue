e52325518bc71fd6d8d8ada762dd6ce8efc00edf
ljourm <ljourm@gmail.com>
2020-09-26 00:56:46 +0900

    feat: formにaria-labelを追加

--- app/javascript/components/molecules/UserForm.vue
+++ app/javascript/components/molecules/UserForm.vue
@@ -3,19 +3,24 @@
     .field
       label.label 氏名
       .control
-        input.input(v-model="form.name", type="text", name="name")
+        input.input(v-model="form.name", type="text", name="name", aria-label="name")
     .field
       label.label EMail
       .control
-        input.input(v-model="form.email", type="text", name="email")
+        input.input(v-model="form.email", type="text", name="email", aria-label="email")
     .field
       label.label パスワード
       .control
-        input.input(v-model="form.password", type="password", name="password")
+        input.input(v-model="form.password", type="password", name="password", aria-label="password")
     .field
       label.label パスワード確認
       .control
-        input.input(v-model="form.password_confirmation", type="password", name="password_confirmation")
+        input.input(
+          v-model="form.password_confirmation",
+          type="password",
+          name="password_confirmation",
+          aria-label="password_confirmation"
+        )
     .field.is-grouped
       .control
         button.button.is-primary.submit(type="submit") 送信
