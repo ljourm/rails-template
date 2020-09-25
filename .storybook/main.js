866e047d99c47fb7533dc5a05630f7a72f1245e3
ljourm <ljourm@gmail.com>
2020-09-25 22:55:56 +0900

    feat: @storybook/addon-a11yの導入

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require("path")
 
 module.exports = {
   stories: ["../app/javascript/**/__stories__/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y"],
   webpackFinal: async (config) => {
     config.module.rules.push(
       {
